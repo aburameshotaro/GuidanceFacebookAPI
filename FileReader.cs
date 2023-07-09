2fbcade8330fe5fb772eaed7dfabaeb15d3806b6
jashi <jashi@LAPTOP-PR8PG6GA>
2023-07-09 15:06:10 +0200

    Dodaj pliki projektów.

---
+++ FileReader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuidanceFacebookAPI
+{
+    internal class FileReader
+    {
+        public static string GuidanceForGivenDate(int month, int day)
+        {
+            string text = File.ReadAllText($"porady/porada{month:00}{day:00}.txt");
+            return text;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GuidanceFacebookAPI;
+
+DateTime startDate = new DateTime(2023,7,9);
+DateTime endDate = new DateTime(2023, 7, 31);
+
+for(int i = 0; i<= (endDate - startDate).Days; i++)
+{
+    DateTime currentDay = startDate.AddDays(i);
+    string guidanceForDay = FileReader.GuidanceForGivenDate(currentDay.Month, currentDay.Day);
+    
+}
